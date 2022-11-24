179ce1972cbef6c6985e808f42e3051aef0dfaff
Greg Hunt <greg@motto.ca>
2022-11-24 17:16:53 -0500

    Disabled SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://greghunt.dev',
   integrations: [mdx(), sitemap()],
-  output: "server",
-  adapter: cloudflare()
+  // output: "server",
+  // adapter: cloudflare()
 });
