7a008c8f117afe706bd384bc44ac224a77303d12
Greg Hunt <greg@motto.ca>
2022-12-12 16:50:54 -0500

    Used tailwindui template

---
+++ src/config.js
@@ -0,0 +1,38 @@
+export const LINKS = [
+    {
+        label: "Twitter",
+        link: "https://twitter.com/greghuntdev",
+    },
+    {
+        label: "Github",
+        link: "https://github.com/greghunt",
+    },
+    {
+        label: "LinkedIn",
+        link: "https://www.linkedin.com/in/greghunt/",
+    },
+    {
+        label: "WordPress Plugins",
+        link: "https://profiles.wordpress.org/freshbrewedweb/#content-plugins",
+    },
+    {
+        label: "Instagram",
+        link: "https://www.instagram.com/gregandboo/",
+    },
+    {
+        label: "Motto Articles",
+        link: "https://motto.kinsta.cloud/author/greg/feed/",
+    },
+    {
+        label: "Hacker News",
+        link: "https://news.ycombinator.com/submitted?id=greghunt",
+    },
+    {
+        label: "Reddit",
+        link: "https://www.reddit.com/user/freshbrewedweb/",
+    },
+    {
+        label: "Stack Overflow",
+        link: "https://stackoverflow.com/users/7875734/greg-hunt",
+    },
+];
--- src/config.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Greg Hunt, Web Developer';
-export const SITE_DESCRIPTION = 'Documenting my life, projects and ideas here.';
